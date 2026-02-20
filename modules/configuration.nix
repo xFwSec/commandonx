cd1cc0af32988f6e0cac65fa083767520bf1f9f2
xFwSec <168570425+xFwSec@users.noreply.github.com>
2026-02-20 14:43:36 +0000

    Update configuration.nix

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -10,6 +10,9 @@
 	imports = [
 		/etc/nixos/hardware-configuration.nix
 	];
+# Bootloader
+boot.loader.systemd-boot.enable = true;
+boot.loader.canTouchEfiVariables = true;
 
 	networking.hostName = "nixos"; # Define your hostname.
 # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
